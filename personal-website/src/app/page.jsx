cc33339afed45da928823d7f1c5f834e95e1b088
cmatheny6 <cmatheny6@gatech.edu>
2025-09-19 21:06:48 -0400

    computer graphic is showing up and looks right. not responsive and file names are bad

--- personal-website/src/app/page.jsx
+++ personal-website/src/app/page.jsx
@@ -1,5 +1,6 @@
 import Name from "./components/Name";
 import Title from "./components/Title";
+import Image from 'next/image'
 import "./globals.css";
 
 export default function Home() {
@@ -7,6 +8,11 @@ export default function Home() {
     <>
       <Name name = "claire matheny"> </Name>
       <Title title ="web dev"> </Title>
+      <div>
+        <Image src="/images/Frame11.png"alt="top of computer" width={1072} height={78.17} className="centered-image top-image"></Image>
+        <Image src="/images/Frame12.png"alt="bottom of computer" width={1072} height={547.17} className="centered-image bottom-image"></Image>
+      </div>
     </>
+
   );
 }
